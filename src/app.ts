d7a9fffeec5c858d80db9bf9079e37c696f78a58
Shohel-Raj <shohel87.dev@gmail.com>
2026-01-26 22:49:18 +0600

    better Auth with basic config

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
 import express from "express"
 
 import cors from "cors"
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app = express();
+
+app.use(cors({
+    origin: process.env.APP_URL ,
+    credentials: true
+}))
+
+app.use(express.json());
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+app.get("/", (req, res) => {
+    res.send("MediStore is cooking..........!");
+});
+
 
 export default app;
